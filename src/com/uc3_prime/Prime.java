5e7a8a6f3c0968b4be1b54936dc42db815aba5b3
Sourav2707 <smartsourav33@gmail.com>
2022-12-16 20:07:14 +0530

    UC3 checking whether the given input is prime or not

---
+++ src/com/uc3_prime/Prime.java
@@ -0,0 +1,25 @@
+package com.uc3_prime;
+import java.util.Scanner;
+public class Prime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n =sc.nextInt();
+        int a = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                a++;
+            }
+        }
+        if (a == 2)
+        {
+            System.out.println(n+" is a Prime Number");
+        }
+        else
+        {
+            System.out.println(n+" is Not a Prime Number");
+        }
+    }
+}
